adeef19f848a5748ed7513f9a54c3a7c9d3db19e
TEHILA AMRAM <tehila@upstream.auto>
2020-05-13 12:09:38 +0300

    reset password server mail

--- routes/index.js
+++ routes/index.js
@@ -2,35 +2,32 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var User = require('../models')("User");
+var nodemailer = require('nodemailer');
+var waterfall = require('async-waterfall');
 const crypto = require('crypto');
 
-// router.get('/', function (req, res, next) {
-//   res.end();
-//   // res.render('index', { title: 'Home' });
-// });
+var serverEmailAddress = 'buy.a.luxury.vehicle';
+var serverEmailPassword = 'tehila1997';
+
 router.post('/signUp', function (req, res, next) {
   var mykey = crypto.createDecipher('aes-128-cbc', 'luxury');
   var mystr = mykey.update(req.body.password, 'hex', 'utf8');
   mystr += mykey.final('utf8');
   User.register(new User({ email: req.body.email, role: 'user', fullName: req.body.fullName }), mystr, function (err, user) {
     console.log('in', user);
-    // console.log(req.session);
     if (err) {
-      console.log('error while user register!', err);
       return res.sendStatus(409);
     }
     req.login(user, function (err) {
       if (err) {
         return res.sendStatus(err);
       }
-      // console.log(req.user);
       return res.json({
         status: 200,
         user: {
           id: req.user.id,
         }
       });
-
     });
   });
 });
@@ -39,58 +36,94 @@ router.post('/signIn', function (req, res) {
   var mystr = mykey.update(req.body.user.password, 'hex', 'utf8');
   mystr += mykey.final('utf8');
   req.body.user.password = mystr;
-    console.log(req.body.user);
   req.login(req.body.user, function (err) {
-    console.log('in login', err);
     if (err) {
       return res.sendStatus(err);
     }
-    console.log(req.user);
-    // User.findById(id, function (err, user) {
-    //   done(err, user);
-    // });
     return User.find({email: req.user.email}).then((fuser) => {
-      console.log(fuser[0].id, ' usersd');
       return res.json({
         status: 200,
         user: {
-          // t: 'ere',
           id: fuser[0].id,
           fullName: fuser[0].fullName,
           role: fuser[0].role,
         }
       });
     });
-  
-
-  })  // res.redirect('/');
-});
-// router.post('/signIn', )
-// GET /logout
-// router.get('/signOut', function (req, res, next) {
-//   if (req.session) {
-//     // delete session object
-//     req.session.destroy(function (err) {
-//       if (err) {
-//         return next(err);
-//       } else {
-//         return res.redirect('/');
-//       }
-//     });
-//   }
-// });
-
-router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), function (req, res) {
-  res.redirect('/');
+  });
 });
 
 router.get('/signOut', function (req, res) {
   console.log(req.session, ' req');
   req.logout();
   return res.sendStatus(200);
+});
 
-  // console.log(req.session, ' req');
-  // res.redirect('/');
+router.post('/resetPassword', async (req, res) => {
+  waterfall([
+    function(done) {
+      crypto.randomBytes(20, (err, buf) => {
+        var token = buf.toString('hex');
+        console.log(token, ' token')
+        done(err, token);
+      });
+    },
+    function(token, done) {
+     User.findOne({email: req.body.email}).exec().then((user) => {
+       console.log(user, ' user reset');
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+        console.log(user, ' user')
+        User.collection.updateOne({email: req.body.email}, user);
+        console.log(user, ' save error')
+        user.save((err) => {
+          console.log(err, ' err save')
+          done(err, token, user);
+        });
+     });
+
+    },
+    function(token, user, done) {
+      console.log('email things')
+      var smtpTrans = nodemailer.createTransport({
+        service: 'Gmail', 
+        auth: {
+         user: serverEmailAddress,
+         pass: serverEmailPassword,
+       }
+     });
+     var mailOptions = {
+
+       to: user.email,
+       from: 'Buy a Luxury Vehicle',
+       subject: 'Reset Password',
+       text:          'Hello ' + user.fullName + '\n\n' +
+       'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+         'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+         'http://' + req.headers.origin + '/reset/' + token + '\n\n' +
+         'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+
+     };
+     console.log('step 3')
+       smtpTrans.sendMail(mailOptions, function(err) {
+       req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+       console.log('sent')
+       if (err) {
+         return res.sendStatus(404);
+       }
+       return res.sendStatus(200);
+      //  res.redirect('/forgot');
+});
+    }
+  ], function(err) {
+    if (err) {
+       res.redirect('/signup');
+   }
+    else{
+      res.redirect('/login');
+    }
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
